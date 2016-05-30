301cf16855032d3ed707ee80a730637ecd191ae1
Levente Szasz <leventeszasz@Attilas-Mac-Pro.local>
2016-05-30 08:55:03 -0700

    Initial Commit

---
+++ Bonodom_IOS_0.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bonodom_IOS_0.1
+//
+//  Created by Levente Szasz on 30/05/16.
+//  Copyright (c) 2016 Levente Szasz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
