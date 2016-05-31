725ba489903f38166bf144529e145ccf6d1953c1
Levente Szasz <leventeszasz@Attilas-Mac-Pro.local>
2016-05-31 04:23:39 -0700

    izé izé

---
+++ Bonodom_IOS_0.1/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Bonodom_IOS_0.1
+//
+//  Created by Levente Szasz on 31/05/16.
+//  Copyright (c) 2016 Levente Szasz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
