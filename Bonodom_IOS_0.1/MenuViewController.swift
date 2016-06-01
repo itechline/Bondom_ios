d28a77efefc2cb309a621dc602b5b34581ee443c
Attila Dan <dn.attila@icloud.com>
2016-06-01 13:45:50 +0200

    side menu

---
+++ Bonodom_IOS_0.1/MenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuViewController.swift
+//  Bonodom_IOS_0.1
+//
+//  Created by Attila Dán on 2016. 06. 01..
+//  Copyright © 2016. Levente Szasz. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
